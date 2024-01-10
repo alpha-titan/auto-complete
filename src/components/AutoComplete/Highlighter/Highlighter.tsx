4687515df8bc7f90100e3fe76b1ee5ff1a5e6845
alpha-titan <alphaxtitan@gmail.com>
2024-01-11 04:37:40 +0530

    refactor Highlight

--- src/components/AutoComplete/Highlighter/Highlighter.tsx
+++ src/components/AutoComplete/Highlighter/Highlighter.tsx
@@ -10,14 +10,18 @@ const Highlighter: React.FC<IHighlighterProps> = ({ query, text }) => {
   const regex = new RegExp(`(${query})`, "gi");
   const parts = text.split(regex);
 
-  return parts.map((part, index) =>
-    regex.test(part) ? (
-      <span key={index} className="highlight">
-        {part}
-      </span>
-    ) : (
-      <span key={index}>{part}</span>
-    )
+  return (
+    <>
+      {parts.map((part, index) =>
+        regex.test(part) ? (
+          <span key={index} className="highlight">
+            {part}
+          </span>
+        ) : (
+          <span key={index}>{part}</span>
+        )
+      )}
+    </>
   );
 };
 
