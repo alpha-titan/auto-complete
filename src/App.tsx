13126b383456e1a721af8f442c2ec3e7f6f438d9
alpha-titan <alphaxtitan@gmail.com>
2024-01-10 22:24:35 +0530

    refactor app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,52 +12,48 @@ function App() {
    *
    */
 
-  const fetchDataAsync = useCallback(
-    async (query: string): Promise<string[]> => {
-      if (!query) {
-        return [];
-      }
-
-      try {
-        // Fetch data from a sample API (JSONPlaceholder)
-        const response = await fetch(
-          `https://jsonplaceholder.typicode.com/posts?title_like=${query}`
-        );
-        const data: Post[] = await response.json();
-
-        // Extract titles from the fetched data
-        const titles = data.map((post) => post.title);
-        return titles;
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        return [];
-      }
-    },
-    []
-  );
-
   // const fetchDataAsync = useCallback(
   //   async (query: string): Promise<string[]> => {
   //     if (!query) {
   //       return [];
   //     }
-  //     const mockData = [
-  //       "apple",
-  //       "banana",
-  //       "cherry",
-  //       "date",
-  //       "elderberry",
-  //       "fig",
-  //     ];
-  //     const filteredData = mockData.filter((item) => item.includes(query));
-  //     return filteredData;
+
+  //     try {
+  //       // Fetch data from a sample API (JSONPlaceholder)
+  //       const response = await fetch(
+  //         `https://jsonplaceholder.typicode.com/posts?title_like=${query}`
+  //       );
+  //       const data: Post[] = await response.json();
+
+  //       // Extract titles from the fetched data
+  //       const titles = data.map((post) => post.title);
+  //       return titles;
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //       return [];
+  //     }
   //   },
   //   []
   // );
 
-  // const getItemKey = useCallback((ele: string) => {
-  //   return ele;
-  // }, []);
+  const fetchDataAsync = useCallback(
+    async (query: string): Promise<string[]> => {
+      if (!query) {
+        return [];
+      }
+      const mockData = [
+        "apple",
+        "banana",
+        "cherry",
+        "date",
+        "elderberry",
+        "fig",
+      ];
+      const filteredData = mockData.filter((item) => item.includes(query));
+      return filteredData;
+    },
+    []
+  );
 
   return (
     <div className="appContainer">
